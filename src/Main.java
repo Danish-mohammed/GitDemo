ce3a6d1475f2479f3e685e78b2692abb8d2a36ad
MohammedAtif-BL <98643253+MohammedAtif-BL@users.noreply.github.com>
2023-11-22 14:48:38 +0530

    Added Functionallity of addtion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Demo{
+    void add(){
+        int num1 = 4;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println(sum);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        System.out.println("Hello world!");
+        obj.add();
+    }
+}
+
